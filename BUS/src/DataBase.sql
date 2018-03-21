48dd6fc5e08e5ad380893f1c33a040c809efade0
ImaneZer <imane.zerouali.pro@gmail.com>
2018-03-21 05:51:41 +0100

    Modification: DataBase

--- BUS/src/DataBase.sql
+++ BUS/src/DataBase.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.1.2deb1ubuntu0.2
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 12, 2018 at 07:52 PM
--- Server version: 5.0.75
--- PHP Version: 5.2.6-3ubuntu4.6
+-- Client :  localhost
+-- Généré le :  Mer 21 Mars 2018 à 05:48
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,47 +17,56 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `azamouche`
+-- Autheur :  `Imane ZEROUALI & Ali ZAMOUCHE`
+--
+--
+-- Base de données :  `data`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Authentification`
+-- Structure de la table `authentification`
 --
 
-CREATE TABLE IF NOT EXISTS `Authentification` (
-  `id_client` int(11) NOT NULL auto_increment,
-  `login` varchar(30) NOT NULL,
-  `psw` varchar(100) NOT NULL,
-  PRIMARY KEY  (`id_client`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+CREATE TABLE IF NOT EXISTS `authentification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(25) NOT NULL,
+  `psw` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `Authentification`
+-- Contenu de la table `authentification`
 --
 
+INSERT INTO `authentification` (`id`, `login`, `psw`) VALUES
+(1, 'AP', '0fd3f8dd5edc33b28db1162e15e8fcbc'),
+(2, 'UC', '6a2fb30a2dacdda4d74122697262a8b7'),
+(3, 'CAPTEURS', 'c0cf7a3019eb553aebe1dd7d9b37c749');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data`
+-- Structure de la table `data`
 --
 
 CREATE TABLE IF NOT EXISTS `data` (
-  `id` int(25) NOT NULL auto_increment,
-  `altitude` varchar(30) NOT NULL,
-  `attitude` varchar(50) NOT NULL,
-  `coordonnee` varchar(30) NOT NULL,
-  `alert` varchar(30) NOT NULL,
-  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(25) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
--- Dumping data for table `data`
+-- Contenu de la table `data`
 --
 
+INSERT INTO `data` (`id`, `type`, `value`, `time`) VALUES
+(2, 'altitude', '987', '2018-03-14 02:17:13'),
+(5, 'coordonnee', '{"x": 23, "y": 12}', '2018-03-16 09:10:17'),
+(17, 'attitude', '{"roll":12,"pitch":20,"yaw":11}', '2018-03-21 04:31:56');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
