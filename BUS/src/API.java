41ddc7f012d0df6aae8ff33f46f72661c26fe7a1
ImaneZer <imane.zerouali.pro@gmail.com>
2018-03-22 05:05:46 +0100

    Factorisation (IHM, Serveur)

--- BUS/src/API.java
+++ BUS/src/API.java
@@ -1,37 +1,29 @@
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.text.AttributeSet;
-import javax.swing.text.DefaultCaret;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 
-import java.awt.*;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.swing.border.Border;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
 /**
  *@authors ZAMOUCHE & ZEROUALI
  */
-
-public class Server {
-
-    //Declaration des variables
-    private static ServerSocket serverSocket=null;
-    private static Socket socket=null;
-    private static DataInputStream in = null;
-    private static PrintStream out = null;
-    private static int maxConnect=10;
-    private static AcceptClient [] AClient =new AcceptClient[maxConnect];
 /*
-    //Declaration des variables pour IHM
+	IHM BUS
+*/
+public class API {
+	//Declaration des variables pour IHM
     private static JFrame Fentre=new JFrame();
     private static Container container=new Container();
     private static JTextPane text1=new JTextPane();
@@ -155,62 +147,5 @@ public class Server {
         text2.setEditable(true);
         appendJTextWithColor(text2, s+"\n", VERY_DARK_BLUE);
         text2.setEditable(false);
-    }*/
-
-    /*
-    * creation de server socket qui ecoute sur un port
-    * Acccepter des clients
-    * */
-    public static void server() throws IOException{
-        String message = "";
-        int port = 2000;
-        serverSocket = new ServerSocket(port);
-        Date date = new Date();
-    	SimpleDateFormat shortDateFormat = new SimpleDateFormat("'le' dd/MM/yyyy 'à' hh:mm:ss:SS");
-    	API.settext2("["+shortDateFormat.format(date).toString()+"], Le server est en ecoute sur le port 2000");
-        System.out.println("Le server est en ecoute sur le port 2000");
-
-        while (true){
-            socket =serverSocket.accept(); //
-            in = new DataInputStream(socket.getInputStream());
-            out = new PrintStream(socket.getOutputStream());
-            //Authentification de client
-            String login = "";
-            String mdp;
-            boolean connect=false;
-            while (!connect) {
-                out.println("Entrez votre login.");
-                out.flush();
-                login = in.readLine().trim();
-
-                out.println("Entrez votre mot de passe");
-                out.flush();
-                mdp = in.readLine().trim();
-                if (dbConnexion.verifie(login, mdp) == true) {
-                    connect=true;
-                }
-            }
-
-            API.settext1(login + " est connecté");
-            System.out.println("message " + login);
-
-            //Creation d'un nouveau client
-            for (int i = 0; i < AClient.length; i++) {
-                if (AClient[i] == null) {
-                    (AClient[i] = new AcceptClient(socket, AClient)).start();
-
-                    break;
-                }
-
-            }
-
-        }
-    }
-
-    public static void main(String args[]) throws IOException {
-
-        API.Fentre();
-        server();
-
     }
 }
